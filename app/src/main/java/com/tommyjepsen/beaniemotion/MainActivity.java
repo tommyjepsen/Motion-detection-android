d94cb19c4a72483c7a7219f6247a131d38f6d0fa
Tommy Jepsen <tommyjepsen89@gmail.com>
2015-09-19 17:23:40 +0200

    Updating it to use Preview, and only take pictures when motion is detected

--- app/src/main/java/com/tommyjepsen/beaniemotion/MainActivity.java
+++ app/src/main/java/com/tommyjepsen/beaniemotion/MainActivity.java
@@ -11,16 +11,15 @@ import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.nio.ByteBuffer;
-import java.util.Arrays;
+import java.util.Calendar;
 
 import dk.nodes.utils.NLog;
 import io.socket.client.IO;
 import io.socket.client.Socket;
-import io.socket.emitter.Emitter;
 
 public class MainActivity extends Activity {
 
@@ -33,10 +32,16 @@ public class MainActivity extends Activity {
     private Camera.PictureCallback mCall;
     private byte[] oldImageData;
     private Handler h = new Handler();
-    private int iterateCameraTrigger = 500; //Miliseconds
-    private int diffCountMaxExceed = 20; //Magic number
+    private int iterateCameraTrigger = 0; //Miliseconds
+    private int diffCountMaxExceed = 60; //Magic number
     private Socket mSocket;
+    private TextView text;
     private String ipAddress = "http://192.168.1.209:3000";
+    private Bitmap fullSize;
+    private Bitmap newImageBitmap;
+    private double oldTotal = 0;
+    private double totalTrigger = 0;
+    private boolean takingImage = false;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -45,6 +50,7 @@ public class MainActivity extends Activity {
 
         iv_image = (ImageView) findViewById(R.id.imageView);
         sv = (SurfaceView) findViewById(R.id.surfaceView);
+        text = (TextView) findViewById(R.id.text);
 
         try {
             mSocket = IO.socket(ipAddress);
@@ -57,7 +63,6 @@ public class MainActivity extends Activity {
         sHolder.addCallback(new SurfaceHolder.Callback() {
             @Override
             public void surfaceCreated(SurfaceHolder holder) {
-                Log.d(MainActivity.class.getSimpleName(), "Camera setting up");
 
                 mCamera = Camera.open();
                 try {
@@ -71,22 +76,29 @@ public class MainActivity extends Activity {
 
             @Override
             public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-                Log.d(MainActivity.class.getSimpleName(), "Camera taking first picture");
 
                 parameters = mCamera.getParameters();
-
+                for (Camera.Size s : parameters.getSupportedPictureSizes()) {
+                    parameters.setPictureSize(s.width, s.height);
+                }
                 mCamera.setParameters(parameters);
                 mCamera.startPreview();
+
+                mCamera.setPreviewCallback(new Camera.PreviewCallback() {
+                    @Override
+                    public void onPreviewFrame(byte[] data, Camera camera) {
+                        compareBytes(data);
+                    }
+                });
 
                 mCall = new Camera.PictureCallback() {
                     @Override
                     public void onPictureTaken(byte[] data, Camera camera) {
-                        NLog.d(MainActivity.class.getSimpleName(), "New picture taken");
-                        compareImages(data);
+                        setImage(data);
+
                     }
                 };
 
-                mCamera.takePicture(null, null, mCall);
             }
 
             @Override
@@ -103,38 +115,75 @@ public class MainActivity extends Activity {
     private void attemptSend() {
         try {
             String message = "Different image!";
-
             mSocket.emit("visitor", message);
         } catch (Exception e) {
             NLog.e(e);
         }
     }
 
-    private void iterateCameraTrigger() {
-        h.postDelayed(
-                new Runnable() {
-                    private long time = 0;
+    private void setImage(byte[] data) {
+        fullSize = BitmapFactory.decodeByteArray(data, 0, data.length);
+        iv_image.setImageBitmap(fullSize);
+        takingImage = false;
+        attemptSend();
+    }
 
-                    @Override
-                    public void run() {
-                        mCamera.startPreview();
-                        mCamera.takePicture(null, null, mCall);
-                    }
-                }, iterateCameraTrigger);
+    private void compareBytes(byte[] newByte) {
+
+        double total = 0;
+        for (int i = 0; i < newByte.length; i++) {
+            total += newByte[i];
+        }
+
+        double abs = (Math.abs(oldTotal - total) / newByte.length);
+        NLog.d(MainActivity.class.getSimpleName(), "" + Calendar.getInstance().getTime() + "ABS: " + abs);
+
+        if (abs > 5 && !takingImage) {
+
+            text.setText("DIFFERENT IMAGE, BRAH");
+
+            if (!takingImage) {
+                try {
+                    mCamera.takePicture(null, null, mCall);
+                } catch (Exception e) {
+                    NLog.e(e);
+                }
+            }
+
+        } else {
+            text.setText("SAME IMAGE BRO");
+        }
+
+        oldTotal = total;
+    }
+
+    private void iterateCameraTrigger() {
+//        h.postDelayed(
+//                new Runnable() {
+//                    private long time = 0;
+//
+//                    @Override
+//                    public void run() {
+//        mCamera.startPreview();
+//        mCamera.takePicture(null, null, mCall);
+//                    }
+//                }, iterateCameraTrigger);
     }
 
     private void compareImages(byte[] newImageData) {
         //Make it a bitmap
-        Bitmap fullSize = BitmapFactory.decodeByteArray(newImageData, 0, newImageData.length);
-        Bitmap newImageBitmap = Bitmap.createScaledBitmap(fullSize, 1, 1, false);
+        fullSize = BitmapFactory.decodeByteArray(newImageData, 0, newImageData.length);
+        newImageBitmap = Bitmap.createScaledBitmap(fullSize, 1, 1, false);
 
         iv_image.setImageBitmap(newImageBitmap);
 
         if (oldImageBitmap != null) {
             if (equals(newImageBitmap, oldImageBitmap)) {
                 NLog.d(MainActivity.class.getSimpleName(), "SAME IMAGE");
+                text.setText("SAME IMAGE BRO");
             } else {
                 NLog.d(MainActivity.class.getSimpleName(), "DIFFERENT IMAGE BRO");
+                text.setText("DIFFERENT IMAGE, BRAH");
                 attemptSend();
             }
         }
@@ -149,12 +198,11 @@ public class MainActivity extends Activity {
     }
 
     public boolean equals(Bitmap bitmap1, Bitmap bitmap2) {
-        int p1 = bitmap1.getPixel(0, 0);
-        int p2 = bitmap2.getPixel(0, 0);
-        int calcDiff = Math.abs(Color.red(p1) - Color.red(p2)) + Math.abs(Color.green(p1) - Color.green(p2)) + Math.abs(Color.blue(p1) - Color.blue(p2));
+        int p1_0 = bitmap1.getPixel(0, 0);
+        int p2_0 = bitmap2.getPixel(0, 0);
+        int calcDiff1 = Math.abs(Color.red(p1_0) - Color.red(p2_0)) + Math.abs(Color.green(p1_0) - Color.green(p2_0)) + Math.abs(Color.blue(p1_0) - Color.blue(p2_0));
 
-        NLog.d(MainActivity.class.getSimpleName(), "Calculated difference: " + calcDiff);
-        if (calcDiff < diffCountMaxExceed) {
+        if (calcDiff1 < diffCountMaxExceed) {
             return true;
         } else {
             return false;
